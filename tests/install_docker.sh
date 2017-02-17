303fb3ed187e3ff50fa86a94cd1ddfc0903ce1b8
Jamie Cressey <jamiecressey89@gmail.com>
2017-02-17 21:36:50 +0000

    Revert changes

--- tests/install_docker.sh
+++ tests/install_docker.sh
@@ -10,21 +10,10 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
 	sudo rm -f `which docker`
 
 	set -e
+	sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+	echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+	echo "deb https://apt.dockerproject.org/repo ubuntu-trusty testing" | sudo tee -a /etc/apt/sources.list.d/docker.list
 	sudo apt-get update
-	sudo apt-get install -y --no-install-recommends \
-		linux-image-extra-$(uname -r) \
-		linux-image-extra-virtual
-	sudo apt-get install -y --no-install-recommends \
-		apt-transport-https \
-		ca-certificates \
-		curl \
-		software-properties-common
-	curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
-	sudo add-apt-repository \
-		"deb https://apt.dockerproject.org/repo/ \
-		ubuntu-$(lsb_release -cs) \
-		main"
-	sudo apt-get update
-	sudo apt-get -y install docker-engine
+	sudo apt-get install docker-engine -y --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 	sudo start docker || true
 fi
