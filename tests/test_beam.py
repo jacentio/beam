d46f4917d6c123b3d94ec71362405c36ad614888
Jamie Cressey <jamiecressey89@gmail.com>
2017-02-18 10:09:52 +0000

    Pull images where we need them

--- tests/test_beam.py
+++ tests/test_beam.py
@@ -22,6 +22,7 @@ def test_empty_drivers_list(working_published_beam):
 	assert len(working_published_beam.drivers) == 0
 
 def test_registering_no_published_services(working_published_beam):
+	working_published_beam.dc.images.pull("redis")
 	container = working_published_beam.dc.containers.run("redis", detach=True)
 
 	services = working_published_beam.get_services_to_register(container.attrs)
@@ -31,6 +32,7 @@ def test_registering_no_published_services(working_published_beam):
         assert len(services) == 0
 
 def test_registering_no_exposed_services(working_exposed_beam):
+	working_exposed_beam.dc.images.pull("redis")
 	container = working_exposed_beam.dc.containers.run("redis", detach=True)
 
 	services = working_exposed_beam.get_services_to_register(container.attrs)
