e433ce20837260d701993579f0cb902e49e76dd2
Jamie Cressey <jamiecressey89@gmail.com>
2017-02-18 11:04:28 +0000

    Initial testing of tags and attributes

--- tests/test_beam.py
+++ tests/test_beam.py
@@ -34,15 +34,65 @@ def test_registering_no_published_services(working_published_beam):
 def test_registering_published_services(working_published_beam):
 	container = working_published_beam.dc.containers.run("redis", detach=True, ports={'6379/tcp': 16379})
 
-        print json.dumps(container.attrs)
+	services = working_published_beam.get_services_to_register(container.attrs)
+
+	container.remove(force=True)
+
+        assert len(services) == 1
+
+	service = services[0]
+
+	assert service.port == 16379
+	assert service.proto == "tcp"
+
+	tags = working_published_beam.get_service_tags(service, container.attrs)
+	assert len(tags) == 0
+
+	attrs = working_published_beam.get_service_attributes(service, container.attrs)
+	assert len(attrs.keys()) == 0
+
+def test_registering_published_servicewith_default_tag(working_published_beam):
+	container = working_published_beam.dc.containers.run("redis", detach=True, ports={'6379/tcp': 16379}, labels={"BEAM_TAGS": "foo"})
 
 	services = working_published_beam.get_services_to_register(container.attrs)
 
 	container.remove(force=True)
 
         assert len(services) == 1
-	assert services[0].port == 16379
-	assert services[0].proto == "tcp"
+
+	service = services[0]
+
+	assert service.port == 16379
+	assert service.proto == "tcp"
+
+	tags = working_published_beam.get_service_tags(service, container.attrs)
+	assert len(tags) == 1
+	assert tags[0] == "foo"
+
+	attrs = working_published_beam.get_service_attributes(service, container.attrs)
+	assert len(attrs.keys()) == 0
+
+def test_registering_published_servicewith_default_and_service_tag(working_published_beam):
+	container = working_published_beam.dc.containers.run("redis", detach=True, ports={'6379/tcp': 16379}, labels={"BEAM_TAGS": "foo", "BEAM_6379_TCP_TAGS": "bar"})
+
+	services = working_published_beam.get_services_to_register(container.attrs)
+
+	container.remove(force=True)
+
+        assert len(services) == 1
+
+	service = services[0]
+
+	assert service.port == 16379
+	assert service.proto == "tcp"
+
+	tags = working_published_beam.get_service_tags(service, container.attrs)
+	assert len(tags) == 2
+	assert "foo" in tags
+	assert "bar" in tags
+
+	attrs = working_published_beam.get_service_attributes(service, container.attrs)
+	assert len(attrs.keys()) == 0
 
 def test_registering_no_exposed_services(working_exposed_beam):
 	container = working_exposed_beam.dc.containers.run("redis", detach=True)
@@ -52,5 +102,14 @@ def test_registering_no_exposed_services(working_exposed_beam):
 	container.remove(force=True)
 
         assert len(services) == 1
+
+	service = services[0]
+
 	assert services[0].port == 6379
 	assert services[0].proto == "tcp"
+
+	tags = working_exposed_beam.get_service_tags(service, container.attrs)
+	assert len(tags) == 0
+
+	attrs = working_exposed_beam.get_service_attributes(service, container.attrs)
+	assert len(attrs.keys()) == 0
